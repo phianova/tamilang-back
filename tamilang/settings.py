2a27497992657c64873fc8fd133d7d4e04f3c993
phianova <98988580+phianova@users.noreply.github.com>
2024-04-29 16:52:45 +0100

    more setup

--- tamilang/settings.py
+++ tamilang/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'corsheaders'
+    'corsheaders',
 
     'tamilangLetters',
 ]
--- tamilangLetters/admin.py
+++ tamilangLetters/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Letter, UserLearning
+
+admin.site.register(Letter)
+admin.site.register(UserLearning)
